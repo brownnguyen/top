4e4e30349871f7045456eadf3610c7ae88fb53d2
unknown <brownnguyen>
2020-08-28 10:15:52 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $('.mv-slide').slick({
     arrows: false
 });
+$('.business-content').slick({
+    arrows: false,
+    slidesToShow: 3,
+    arrows: false
+});
+$('.people-content').slick({
+    arrows: false,
+    slidesToShow: 5,
+    arrows: false
+});
