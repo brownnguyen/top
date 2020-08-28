3b1bfb6415516a37e6886709ee53cc518cbe2728
unknown <brownnguyen>
2020-08-28 17:12:30 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 $('.mv-slide').slick({
-    arrows: false
-});
-$('.business-content').slick({
     arrows: false,
-    slidesToShow: 3,
-    arrows: false
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+});
+$('.business-block-1').slick({
+    arrows: false,
+    slidesToShow: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.business-block-2'
+});
+$('.business-block-2').slick({
+    arrows: false,
+    slidesToShow: 2,
+    arrows: false,
+    asNavFor: '.business-block-1'
 });
 $('.people-content').slick({
     arrows: false,
