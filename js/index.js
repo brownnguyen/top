77d0d0954e6c4de9a413f3de38954fb58f84a4ea
unknown <brownnguyen>
2020-08-31 14:31:43 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,24 +1,83 @@
-$('.mv-slide').slick({
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    fade: true,
+$(".mv-slide").slick({
+  arrows: false,
+  //   autoplay: true,
+  autoplaySpeed: 4000,
+  fade: true,
 });
-$('.business-block-1').slick({
-    arrows: false,
-    slidesToShow: 1,
-    arrows: true,
-    fade: true,
-    asNavFor: '.business-block-2',
+$(".business-block-1").slick({
+  slidesToShow: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: ".content-block-2-main",
 });
-$('.business-block-2').slick({
-    arrows: false,
-    slidesToShow: 2,
-    arrows: false,
-    asNavFor: '.business-block-1'
+$(".content-block-2-main").slick({
+  arrows: false,
+  slidesToShow: 2,
+  asNavFor: ".business-block-1",
 });
-$('.people-content').slick({
-    arrows: false,
-    slidesToShow: 5,
-    arrows: false
+$(".people-content").slick({
+  arrows: true,
+  slidesToShow: 5,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
+});
+let arrowRight = document.querySelector(
+  ".business .business-content .business-block-1 .slick-next"
+);
+let arrowLeft = document.querySelector(
+  ".business .business-content .business-block-1 .slick-prev"
+);
+let header = document.querySelector("header");
+arrowRight.innerHTML =
+  '<div class="arrowRight"><span class="right"></span></div>';
+arrowLeft.innerHTML = '<div class="arrowLeft"><span class="left"></span></div>';
+let menuBurger = document.querySelector(".menu-burger .menu");
+let drawer = document.querySelector(".drawer");
+menuBurger.onclick = function () {
+  menuBurger.classList.toggle("active");
+  if (menuBurger.classList.contains("active")) {
+    drawer.classList.add("expand");
+  } else {
+    drawer.classList.remove("expand");
+  }
+};
+let peopleArrowLeft = document.querySelector(".people-content .slick-prev");
+peopleArrowLeft.innerHTML = '<span class="people-arrow-left"></span>';
+let peopleArrowRight = document.querySelector(".people-content .slick-next");
+peopleArrowRight.innerHTML = '<span class="people-arrow-right"></span>';
+
+window.onscroll = function () {
+  if (window.pageYOffset > window.innerHeight / 2) {
+    document.querySelector("#backTop").style.opacity = 0.8;
+    document.querySelector("#backTop").style.visibility = "visible";
+  } else {
+    document.querySelector("#backTop").style.opacity = 0;
+    document.querySelector("#backTop").style.visibility = "hidden";
+  }
+};
+$("#backTop").click(function (event) {
+  $("html,body").animate(
+    {
+      scrollTop: 0,
+    },
+    1000,
+    "swing"
+  );
+  return false;
 });
